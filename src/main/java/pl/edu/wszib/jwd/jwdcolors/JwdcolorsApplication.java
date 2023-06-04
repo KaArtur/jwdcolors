1a9975e93e4ddd38b068582d18fa2b725dbf4172
Artur <arturkawa111@gmail.com>
2023-06-04 11:01:39 +0200

    Properties

--- src/main/java/pl/edu/wszib/jwd/jwdcolors/JwdcolorsApplication.java
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/JwdcolorsApplication.java
@@ -2,7 +2,10 @@ package pl.edu.wszib.jwd.jwdcolors;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
+@PropertySource("classpath:db.properties")
 @SpringBootApplication
 public class JwdcolorsApplication {
 
